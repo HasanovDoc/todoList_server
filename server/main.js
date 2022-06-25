d164f8fee016fe7986f5bb42db4f2f471ea7fe6c
GizzaProgger <147rawil147@gmail.com>
2022-06-25 18:29:04 +0400

    первая версия CRUD для todolist

--- server/main.js
+++ server/main.js
@@ -1,11 +1,123 @@
-const html_url = require('../client/index.html');
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'test/html' });
-    res.end(``);
-});
+// const html_url = require('../client/index.html');
+import mysql from "mysql2/promise";
+import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+import moment from "moment";
 
 
-server.listen(5000, () => {
-    console.log("The server is running");
-});
+const app = express()
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
+
+async function initConnection() {
+    const connection = await mysql.createConnection({
+        // тут надо указать данные для подключения
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        database: "todolist",
+        password: "",
+    });
+    return connection
+}
+
+(async () => {
+    const connection = await initConnection()
+    let d = (date) => moment(parseInt(date)).format("YYYY-MM-DD");
+
+    const addTodo = async ({ title, subtitle, date}) => {
+        // Метод для добавления записи
+        connection.query(
+            `INSERT INTO todolist (title, subtitle, date) VALUES ('${title}', '${subtitle}', '${
+                d(date)
+            }')`
+        )
+    }
+
+    async function getTodo(id) {
+        // метод для получения запии по id
+        let [rows] = await connection.query(
+            `SELECT * FROM todolist WHERE id = '${id}'`
+        )
+        return rows[0]
+    }
+
+    async function getAllTodos(id) {
+        // метод для получения всех записей
+        let [rows] = await connection.query(
+            `SELECT * FROM todolist`
+        )
+        return rows
+    }
+
+    async function updateDone(id) {
+        // метод для обновления статуса элемента из списка на выполненно
+        return connection.query(
+            `UPDATE todolist SET done = '1' WHERE id = '${id}'`
+        )
+    }
+
+    async function deleteTodo(id) {
+        // метод для удаления записи по id
+        return connection.query(
+            `DELETE FROM todolist WHERE id = '${id}'`
+        )
+    }
+
+    app.get("/todo/:id", async (req, res) => {
+        let data = await getTodo(req.params.id)
+        res.json({
+            success: true,
+            data
+        })
+    })
+
+    app.put('/todo/:id', async (req, res) => {
+        updateDone(req.params.id).then(() => {
+            res.json({
+                success: true,
+            }) 
+        }).catch(() => {
+            res.json({
+                success: false,
+            }) 
+        })
+    })
+
+    app.delete('/todo/:id', async (req, res) => {
+        deleteTodo(req.params.id).then(() => {
+            res.json({
+                success: true,
+            }) 
+        }).catch(() => {
+            res.json({
+                success: false,
+            }) 
+        })
+    })
+
+    app.get("/todo/", async (req, res) => {
+        let data = await getAllTodos()
+        res.json({
+            success: true,
+            data
+        })
+    })
+
+    app.post('/todo/', (req, res) => {
+        addTodo(req.query)
+        res.json({
+            success: true,
+        })
+    })
+})()
+
+app.listen(5000, () => {
+    console.log("server is listen 5000 port")
+})
